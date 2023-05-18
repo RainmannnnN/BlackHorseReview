cf100d7ef4d95613a4a178241c799bc40f5ffc66
yhy <2420267525@qq.com>
2023-05-18 15:35:07 +0800

    first

---
+++ src/main/java/com/yhy/blackhorsereview/BlackHorseReviewApplication.java
@@ -0,0 +1,13 @@
+package com.yhy.blackhorsereview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlackHorseReviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlackHorseReviewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yhy/blackhorsereview/BlackHorseReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yhy.blackhorsereview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlackHorseReviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
