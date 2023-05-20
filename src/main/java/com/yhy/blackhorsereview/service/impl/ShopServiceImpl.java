bc201d544a6044334298aab0b4e23485b5b42647
yhy <2420267525@qq.com>
2023-05-20 17:48:17 +0800

    用逻辑过期解决缓存击穿

--- src/main/java/com/yhy/blackhorsereview/service/impl/ShopServiceImpl.java
+++ src/main/java/com/yhy/blackhorsereview/service/impl/ShopServiceImpl.java
@@ -2,6 +2,7 @@ package com.yhy.blackhorsereview.service.impl;
 
 import cn.hutool.core.util.BooleanUtil;
 import cn.hutool.core.util.StrUtil;
+import cn.hutool.json.JSONObject;
 import cn.hutool.json.JSONUtil;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.yhy.blackhorsereview.dto.Result;
@@ -9,6 +10,7 @@ import com.yhy.blackhorsereview.entity.Shop;
 import com.yhy.blackhorsereview.mapper.ShopMapper;
 import com.yhy.blackhorsereview.service.IShopService;
 import com.yhy.blackhorsereview.utils.RedisConstants;
+import com.yhy.blackhorsereview.utils.RedisData;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
@@ -16,6 +18,9 @@ import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
 
+import java.time.LocalDateTime;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 import static com.yhy.blackhorsereview.utils.RedisConstants.*;
@@ -41,11 +46,60 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
         // Shop shop = queryWithPassThrough(id);
 
         // 用互斥锁解决缓存击穿
-        Shop shop = queryWithMuteX(id);
+        // Shop shop = queryWithMuteX(id);
+
+        // 逻辑过期解决缓存击穿
+        Shop shop = queryWithLogicExpire(id);
         if (shop == null) {
             return Result.fail("店铺不存在！");
         }
         return Result.ok(shop);
+    }
+
+    private static final ExecutorService CACHE_REBUILD_EXECUTOR = Executors.newFixedThreadPool(10);
+
+    /**
+     * 逻辑过期解决缓存击穿
+     * @param id
+     * @return
+     */
+    public Shop queryWithLogicExpire(Long id){
+        // 根据id查找redis缓存
+        String key = CACHE_SHOP_KEY + id;
+        String shopJson = stringRedisTemplate.opsForValue().get(key);
+        // 判断是否有缓存
+        if (StrUtil.isBlank(shopJson)){
+            return null;
+        }
+        // 命中，需要先把json反序列化为对象
+        RedisData redisData = JSONUtil.toBean(shopJson, RedisData.class);
+        Shop shop = JSONUtil.toBean(((JSONObject) redisData.getData()), Shop.class);
+        LocalDateTime expireTime = redisData.getExpireTime();
+        // 判断是否过期
+        if (expireTime.isAfter(LocalDateTime.now())) {
+            // 没过期,直接返回商品信息
+            return shop;
+        }
+        // 已过期，需要缓存重建
+        // 获取互斥锁
+        String lockKey = LOCK_SHOP_KEY + id;
+        boolean isLock = tryLock(lockKey);
+        // 判断是否获取锁成功
+        if (isLock) {
+            // 如果成功则开启独立线程，实现缓存重建
+            CACHE_REBUILD_EXECUTOR.submit(() -> {
+                try {
+                    this.saveShop2Redis(id, 1800L);
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                } finally {
+                    // 释放锁
+                    unLock(lockKey);
+                }
+            });
+        }
+        // 返回过期的店铺信息
+        return shop;
     }
 
     /**
@@ -100,6 +154,20 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
         return shop;
     }
 
+    /**
+     * 存储店铺信息逻辑过期时间的函数
+     * @param id
+     * @param expireSeconds
+     */
+    public void saveShop2Redis(Long id, Long expireSeconds){
+        Shop shop = getById(id);
+        // 封装逻辑过期时间
+        RedisData redisData = new RedisData();
+        redisData.setData(shop);
+        redisData.setExpireTime(LocalDateTime.now().plusSeconds(expireSeconds));
+        // 写入redis
+        stringRedisTemplate.opsForValue().set(CACHE_SHOP_KEY + id, JSONUtil.toJsonStr(redisData));
+    }
 
     /**
      * 防止缓存穿透的代码
--- src/test/java/com/yhy/blackhorsereview/BlackHorseReviewApplicationTests.java
+++ src/test/java/com/yhy/blackhorsereview/BlackHorseReviewApplicationTests.java
@@ -1,13 +1,24 @@
 package com.yhy.blackhorsereview;
 
+import com.yhy.blackhorsereview.service.impl.ShopServiceImpl;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
 
 @SpringBootTest
 class BlackHorseReviewApplicationTests {
 
+    @Resource
+    private ShopServiceImpl shopService;
+
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    public void testSaveShop2Redis(){
+        shopService.saveShop2Redis(1L, 10L);
     }
 
 }
